964dafcbbbcfd4446d3e8eab7f3f836b0cce1c61
immusawer <musawerdelair2020@gmail.com>
2025-06-04 19:22:36 +0430

    Add UsersModule to MessagesModule imports

--- src/messages/messages.module.ts
+++ src/messages/messages.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { MessagesService } from './messages.service';
 import { MessagesController } from './messages.controller';
 import { AuthModule } from '../auth/auth.module';
+import { UsersModule } from '../users/users.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, UsersModule],
   controllers: [MessagesController],
   providers: [MessagesService],
 })
